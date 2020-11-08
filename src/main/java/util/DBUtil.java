becd1070f9ce36bf8af110274f731a7ed451375e
eightyninevn <eightyninevn@gmail.com>
2020-11-08 18:01:44 +0700

    q

--- src/main/java/util/DBUtil.java
+++ src/main/java/util/DBUtil.java
@@ -9,18 +9,6 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 
-import javax.swing.JComboBox;
-
-import model.Account;
-import model.Area;
-import model.Department;
-import model.Position;
-import model.Specialize;
-import model.Status;
-import model.WorkShift;
-import modelView.EmployeeView;
-import modelView.RoomView;
-
 public class DBUtil {
 
 	public static DBUtil instance = new DBUtil();
@@ -78,67 +66,5 @@ public class DBUtil {
 		}
 		return -1;
 	}
-	
-	public void Rendercbox_Area(LinkedHashSet<Area> data, JComboBox cbox) {
-		for (Area area : data) {
-			cbox.addItem(area.getName());
-		}
-	}
-	
-	public void Rendercbox_Department (LinkedHashSet<Department> data, JComboBox cbox) {
-		for (Department dt : data) {
-			cbox.addItem(dt.getName());
-		}
-	}
-	
-	
-	public void Rendercbox_Position (LinkedHashSet<Position> data, JComboBox cbPosition) {
-		for (Position dt : data) {
-			cbPosition.addItem(dt.getName());
-		}
-	}
-	
-	public void Rendercbox_Specialize (LinkedHashSet<Specialize> data, JComboBox cbSpecialize) {
-		for (Specialize dt : data) {
-			cbSpecialize.addItem(dt.getName());
-		}
-	}
-	
-	public void Rendercbox_Account (LinkedHashSet<Account> data, JComboBox cbAccount) {
-		for (Account dt : data) {
-			cbAccount.addItem(dt.getName());
-		}
-	}
-	
-	public void Rendercbox_Status (LinkedHashSet<Status> data, JComboBox cbStatus) {
-		for (Status dt : data) {
-			cbStatus.addItem(dt.getName());
-		}
-	}
-	
-	public void Rendercbox_EmployeeView (LinkedHashSet<EmployeeView> data, JComboBox cbEmployeeView) {
-		for (EmployeeView dt : data) {
-			cbEmployeeView.addItem(dt.getName()+"  -  "+dt.getNamePosition()+ "  -  "+dt.getNameDepartment());
-		}
-	}
-	
-	public void Rendercbox_RoomView (LinkedHashSet<RoomView> data, JComboBox cbRoomView) {
-		for (RoomView dt : data) {
-			cbRoomView.addItem(dt.getName());
-		}
-	}
-	
-	public void Rendercbox_WorkShift (LinkedHashSet<WorkShift> data, JComboBox cbWorkShift) {
-		for (WorkShift dt : data) {
-			cbWorkShift.addItem(dt.getName()+":  "+dt.getTime());
-		}
-	}
-
-	
-//	public <T> void Rendercbox (LinkedHashSet<T> data, JComboBox cbox) {
-//		for (T dt : data) {
-//			cbox.addItem(dt.getName());
-//		}
-//	}
 	
 }
